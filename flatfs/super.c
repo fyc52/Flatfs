c3502c8958c40572be642bf9ce6b46d04d0cd89c
huangweizhou <huangweizhou@hust.edu.cn>
2022-05-17 19:40:17 +0800

    updates

--- flatfs/super.c
+++ flatfs/super.c
@@ -4,13 +4,14 @@
 
 static int flatfs_fill_super(struct super_block * sb, void * data, int silent)
 {
+	printk("fill sb of flatfs\n");
 	return 0;
 }
 /*
  * mount flatfs, call kernel util mount_bdev
  * actual work of flatfs is done in flatfs_fill_super
  */
-static struct dentry *flatfs_get_sb(struct file_system_type *fs_type,
+static struct dentry *flatfs_mount(struct file_system_type *fs_type,
         int flags, const char *dev_name, void *data, struct vfsmount *mnt)
 {
 	//return get_sb_nodev(fs_type, flags, data, flatfs_fill_super, mnt);//内存文件系统，无实际设备
@@ -27,12 +28,12 @@ static void flatfs_kill_sb(struct super_block *sb)
 static struct file_system_type flatfs_fs_type = { //文件系统最基本的变量
 	.owner = THIS_MODULE,
 	.name = "flatfs",
-	.mount = flatfs_get_sb,     //创建sb
+	.mount = flatfs_mount,     //创建sb
 	.kill_sb = flatfs_kill_sb,  //删除sb
 	/*  .fs_flags */
 };
 
-static int __init init_flatfs_fs(void) //__init表示该函数旨在初始化期间使用，模块装载后就扔掉，释放内存
+static int __init init_flatfs_fs(void) //宏定义__init表示该函数旨在初始化期间使用，模块装载后就扔掉，释放内存
 {
 	return register_filesystem(&flatfs_fs_type); //内核文件系统API
 }
@@ -42,5 +43,5 @@ static void __exit exit_flatfs_fs(void)
 	unregister_filesystem(&flatfs_fs_type);
 }
 
-module_init(init_flatfs_fs) //模块加载
+module_init(init_flatfs_fs) //宏：模块加载
 module_exit(exit_flatfs_fs)
