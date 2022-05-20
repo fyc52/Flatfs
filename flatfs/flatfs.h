f0d5d32821b63f5276f27237a0ed61a0bddb7b33
huangweizhou <huangweizhou@hust.edu.cn>
2022-05-20 10:42:11 +0800

    updates

--- flatfs/flatfs.h
+++ flatfs/flatfs.h
@@ -1,21 +1,18 @@
-#define FLATFS_ROOT_I 2
+#define SAMPLEFS_ROOT_I 2
 
 /* This is an example of filesystem specific mount data that a file system might
    want to store.  FS per-superblock data varies widely and some fs do not
    require any information beyond the generic info which is already in
    struct super_block */
-struct flatfs_info {
-	ino_t next_ino;
-	ino_t max_ino;
+struct samplefs_sb_info {
+	unsigned int rsize;
+	unsigned int wsize;
+	int mnt_flags;
+	struct nls_table *local_nls;
 };
 
-// LIGHTFS superblock info
-struct flatfs_sb_info {
-	// DB info
-	DB_ENV *db_env;
-	DB *data_db;
-	DB *meta_db;
-	DB *cache_db;
-	unsigned s_nr_cpus;
-	struct flatfs_info __percpu *s_lightfs_info;
-};
+static inline struct samplefs_sb_info *
+SFS_SB(struct super_block *sb)
+{
+	return sb->s_fs_info;
+}
